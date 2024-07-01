8ecc02bfeb149aa29bac431e670c5016ad786821
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-01 15:55:46 +0200

    [UPD]Added IF in script.js in jsnack 3 folder

--- jsnack 3/script.js
+++ jsnack 3/script.js
@@ -4,9 +4,10 @@ let num = prompt("Inserisci un numero di 4 cifre:")
 
 //Verifica che l'input sia un numero di 4 cifre
 
-//IF il numero NON è di 4 cifre 
-
-//Richiedere di mettere un numero da 4 cifre
+if (num.length !== 4){
+    //IF il numero NON è di 4 cifre : Richiedere di mettere un numero da 4 cifre
+    console.log("Inserisci un numero da 4 cifre.")
+}
 
 //ELSE se il numero è di 4 cifre
 
