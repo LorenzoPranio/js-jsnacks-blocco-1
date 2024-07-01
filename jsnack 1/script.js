4a86dd280acb77bf54627f81f5382979593631b7
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-01 14:06:50 +0200

    [UPD]Added scaffolding in script.js in jsnack 1 folder

---
+++ jsnack 1/script.js
@@ -0,0 +1,9 @@
+//Creare un prompt per la richiesta del nome dell'invitato
+
+//Creo una FLAG impostata su FALSE
+
+//Creare un ARREY con la lista dei nomi
+
+//Ciclo l'ARRAY e confronto l'elemento che sto ciclando con il nome inserito nel prompt
+
+//Mostro il messaggio corrispondente a seconda della FLAG
