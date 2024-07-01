f46541c0343d7c2ac36bf1ad473f7b74edf410bd
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-01 14:49:46 +0200

    [UPD]Added scaffolding in script.js in jsnack 2 folder

---
+++ jsnack 2/script.js
@@ -0,0 +1,9 @@
+//Creare un ARREY vuoto
+
+//Creare un ciclo for che richieda per 6 volte:
+
+    //Crea un prompt dove chiedi all'utente di inserire un numero
+
+    //IF il numero è pari NON verrà inserito nell'ARREY
+
+    //ELSE se il numero è dispari verrà inserito nell'ARREY
