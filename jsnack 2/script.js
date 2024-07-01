b27332b0aed4cebec83daaf00232a595789ba0f8
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-01 14:57:54 +0200

    [UPD]Added cicle for and prompt in script.js in jsnack 2 folder

--- jsnack 2/script.js
+++ jsnack 2/script.js
@@ -2,10 +2,17 @@
 
 let disp_number = []
 
-//Creare un ciclo for che richieda per 6 volte:
+//Creare un ciclo for che richieda per 6 volte:ù
 
+for (i = 0; i < 6; i++){
+    
     //Crea un prompt dove chiedi all'utente di inserire un numero
+    let num = parseInt(prompt("Inserisci un numero"));
 
     //IF il numero è pari NON verrà inserito nell'ARREY
 
     //ELSE se il numero è dispari verrà inserito nell'ARREY
+}
+
+    
+
