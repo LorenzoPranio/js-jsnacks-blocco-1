54b3edff0e563d74f37e0c58c785adcb1526cb89
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-01 15:53:04 +0200

    [UPD]Added prompt in script.js in jsnack 3 folder

---
+++ jsnack 3/script.js
@@ -0,0 +1,19 @@
+//Creare un prompt per chiedere all'utente di inserire un numero di 4 cifre
+
+let num = prompt("Inserisci un numero di 4 cifre:")
+
+//Verifica che l'input sia un numero di 4 cifre
+
+//IF il numero NON è di 4 cifre 
+
+//Richiedere di mettere un numero da 4 cifre
+
+//ELSE se il numero è di 4 cifre
+
+//Crea un Cicle For
+
+//Dividi il numero in singole cifre
+
+//Calcola la somma delle cifre
+
+//Fai uscire il tutto in console log
