72a8bc3fe305813874ad0a35b0efb82c1b316d1a
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-01 15:14:55 +0200

    [UPD]Added console log in script.js in jsnack 2 folder

--- jsnack 2/script.js
+++ jsnack 2/script.js
@@ -2,15 +2,18 @@
 
 let disp_number = []
 
+console.log(disp_number)
+
+console.log(disp_number.length)
+
 //Creare un ciclo for che richieda per 6 volte:ù
 
 for (i = 0; i < 6; i++){
-    
+
     //Crea un prompt dove chiedi all'utente di inserire un numero
     let num = parseInt(prompt("Inserisci un numero"));
 
     //IF il numero è pari NON verrà inserito nell'ARREY
-
     //ELSE se il numero è dispari verrà inserito nell'ARREY
 }
 
