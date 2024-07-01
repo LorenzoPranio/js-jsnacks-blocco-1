b65e226d9e345e603a5337a407ee9740bb47149e
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-01 16:06:13 +0200

    [UPD]Added ELSE in script.js in jsnack 3 folder and finished the excercise

--- jsnack 3/script.js
+++ jsnack 3/script.js
@@ -1,20 +1,31 @@
 //Creare un prompt per chiedere all'utente di inserire un numero di 4 cifre
 
-let num = prompt("Inserisci un numero di 4 cifre:")
+let num = prompt("Inserisci un numero di 4 cifre:");
 
 //Verifica che l'input sia un numero di 4 cifre
 
 if (num.length !== 4){
     //IF il numero NON è di 4 cifre : Richiedere di mettere un numero da 4 cifre
-    console.log("Inserisci un numero da 4 cifre.")
+    console.log("Inserisci un numero da 4 cifre.");
 }
 
 //ELSE se il numero è di 4 cifre
 
-//Crea un Cicle For
+else{
+    //Dividi il numero in singole cifre
+    let text = num.split('');
+    //Calcola la somma delle cifre con un Cicle For
+    let somma = 0;
+    for (let i = 0; i < text.length; i++){
+        somma += parseInt(text[i]); 
+    }
+   //Fai uscire il tutto in console log
+   console.log('la somma è: ' + somma) 
+}
 
-//Dividi il numero in singole cifre
 
-//Calcola la somma delle cifre
 
-//Fai uscire il tutto in console log
+
+
+
+
