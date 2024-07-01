e6e954da8fb3e18fff30d2a56c4085eba28217a6
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-01 15:27:53 +0200

    [UPD]Added push of odd number and added console log in script.js in jsnack 2 folder and finished the excercise

--- jsnack 2/script.js
+++ jsnack 2/script.js
@@ -2,10 +2,6 @@
 
 let disp_number = []
 
-console.log(disp_number)
-
-console.log(disp_number.length)
-
 //Creare un ciclo for che richieda per 6 volte:ù
 
 for (i = 0; i < 6; i++){
@@ -13,9 +9,13 @@ for (i = 0; i < 6; i++){
     //Crea un prompt dove chiedi all'utente di inserire un numero
     let num = parseInt(prompt("Inserisci un numero"));
 
-    //IF il numero è pari NON verrà inserito nell'ARREY
-    //ELSE se il numero è dispari verrà inserito nell'ARREY
+    //IF il numero è pari NON verrà inserito nell'ARREY se il numero è dispari verrà inserito nell'ARREY
+    if (num % 2 !== 0){
+        disp_number.push(num);
+    }
 }
+
+console.log("Numeri dispari inseriti:", disp_number);
 
     
 
